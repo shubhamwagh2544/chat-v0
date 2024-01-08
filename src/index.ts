103d78c37bba21549dd8f642034dd1c379dbe5d9
Shubham Wagh <shubhamwagh2544@gmail.com>
2024-01-08 11:49:53 +0530

    init ws repo

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import {server as WebSocektServer} from "websocket";
+import http from "http";
+
+var server = http.createServer(function(request: any, response: any){
+    console.log(new Date()+ "received request for" + request.url);
+    response.writeHead(404);
+    response.end();
+});
+
+server.listen(8080, function() {
+    console.log(new Date() + "server is listening on port 8080");
+});
+
+const wsServer = new WebSocektServer({
+    httpServer: server,
+    autoAcceptConnections: false
+});
+
+function originIsAllowed(origin: string) {
+    return true;
+}
+
+wsServer.on('request', function(request) {
+    if (!originIsAllowed(request.origin)) {
+      // Make sure we only accept requests from an allowed origin
+      request.reject();
+      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+      return;
+    }
+    
+    var connection = request.accept('echo-protocol', request.origin);
+    console.log((new Date()) + ' Connection accepted.');
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log('Received Message: ' + message.utf8Data);
+            connection.sendUTF(message.utf8Data);
+        }
+        else if (message.type === 'binary') {
+            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+            connection.sendBytes(message.binaryData);
+        }
+    });
+    connection.on('close', function(reasonCode, description) {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    });
+});
