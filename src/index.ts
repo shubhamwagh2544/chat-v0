80b6cd76f7b56154160a099da115cd0e310e6c2e
Shubham Wagh <shubhamwagh2544@gmail.com>
2024-02-10 17:49:00 +0530

    start-1

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import {server as WebSocektServer} from "websocket";
+import { server as WebSocektServer } from "websocket";
 import http from "http";
 
-var server = http.createServer(function(request: any, response: any){
-    console.log(new Date()+ "received request for" + request.url);
+let server = http.createServer(function (request: any, response: any) {
+    console.log(new Date() + "received request for" + request.url);
     response.writeHead(404);
     response.end();
 });
 
-server.listen(8080, function() {
+server.listen(8080, function () {
     console.log(new Date() + "server is listening on port 8080");
 });
 
@@ -20,17 +20,17 @@ function originIsAllowed(origin: string) {
     return true;
 }
 
-wsServer.on('request', function(request) {
+wsServer.on('request', function (request) {
     if (!originIsAllowed(request.origin)) {
-      // Make sure we only accept requests from an allowed origin
-      request.reject();
-      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-      return;
+        // Make sure we only accept requests from an allowed origin
+        request.reject();
+        console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+        return;
     }
-    
-    var connection = request.accept('echo-protocol', request.origin);
+
+    let connection = request.accept('echo-protocol', request.origin);
     console.log((new Date()) + ' Connection accepted.');
-    connection.on('message', function(message) {
+    connection.on('message', function (message) {
         if (message.type === 'utf8') {
             console.log('Received Message: ' + message.utf8Data);
             connection.sendUTF(message.utf8Data);
@@ -40,7 +40,7 @@ wsServer.on('request', function(request) {
             connection.sendBytes(message.binaryData);
         }
     });
-    connection.on('close', function(reasonCode, description) {
+    connection.on('close', function (reasonCode, description) {
         console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
     });
 });
